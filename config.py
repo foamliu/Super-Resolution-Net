2d81bf4fc31f3eeeec998ed4370003898cb34661
Yang Liu <foamliu@yeah.net>
2018-06-08 17:05:25 +0800

    update

---
+++ config.py
@@ -0,0 +1,14 @@
+img_rows, img_cols = 256, 256
+channel = 3
+batch_size = 160
+epochs = 10000
+patience = 50
+num_train_samples = 884560
+num_valid_samples = 7134
+num_classes = 313
+epsilon = 1e-6
+epsilon_sqr = epsilon ** 2
+
+nb_neighbors = 5
+# temperature parameter T
+T = .14
