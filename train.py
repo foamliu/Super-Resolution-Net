9d57d50e60cb9012f89fc0b44891d99e09fc9892
Yang Liu <foamliu@yeah.net>
2018-07-23 20:30:00 +0800

    update

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    model_names = checkpoint_models_path + 'model.x' + scale + '-{epoch:02d}-{val_loss:.4f}.hdf5'
+    model_names = checkpoint_models_path + 'model.x' + str(scale) + '-{epoch:02d}-{val_loss:.4f}.hdf5'
     model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
     early_stop = EarlyStopping('val_loss', patience=patience)
     reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
