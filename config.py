aac42792f196239603adfb8d86cd26d1ea32a91f
Yang Liu <foamliu@yeah.net>
2018-08-10 10:00:31 +0800

    update models

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ epsilon_sqr = epsilon ** 2
 image_folder = '/mnt/code/ImageNet-Downloader/image/resized'
 eval_path = 'eval.json'
 
-best_model = {'x2': 'model.x2-09-4.0598.hdf5', 'x3': 'model.x3-07-6.8288.hdf5', 'x4': 'model.x4-09-8.1142.hdf5'}
+best_model = {'x2': 'model.x2-03-3.9841.hdf5', 'x3': 'model.x3-10-6.6455.hdf5', 'x4': 'model.x4-09-7.9866.hdf5'}
 
